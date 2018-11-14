bba560f388f9ba2b8eb49b94b45080c4c8dbdb1d
Akai9866 <b16036@students.iitmandi.ac.in>
2018-11-14 20:49:15 +0530

    Add files via upload

--- src/devices/timer.c
+++ src/devices/timer.c
@@ -90,10 +90,14 @@ void
 timer_sleep (int64_t ticks) 
 {
   int64_t start = timer_ticks ();
-
+  int64_t wakeup_at = start + ticks;
   ASSERT (intr_get_level () == INTR_ON);
-  while (timer_elapsed (start) < ticks) 
-    thread_yield ();
+  thread_priority_temporarily_up();
+  thread_block_till(wakeup_at, before);
+  /*while (timer_elapsed (start) < ticks) 
+    thread_yield ();*/
+  thread_set_next_wakeup();
+  thread_priority_restore();
 }
 
 /* Sleeps for approximately MS milliseconds.  Interrupts must be
